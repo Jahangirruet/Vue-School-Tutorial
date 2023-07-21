3bb0c41e71030776943719cca96e7fd858c8ada1
Md. Jahangir Alam <jahangir.2012@outlook.com>
2023-07-21 19:50:35 +0600

    composition api completed

--- Vue-School-Tutorial/src/App.vue
+++ Vue-School-Tutorial/src/App.vue
@@ -8,34 +8,6 @@ components: { NavigationView}
 
 <template>
   <div>
-    <!-- <header>
-      <div>
-        <nav>
-          <ul>
-            <li><RouterLink to="/">Vue School Tutorial</RouterLink></li>
-            <li>
-              Composition APi
-              <ul>
-                <li><RouterLink to="/CompositionAPI/lession1"> One</RouterLink></li>
-                <li><RouterLink to="/CompositionAPI/lession2"> Two </RouterLink></li>
-                <li><RouterLink to="/CompositionAPI/lession3"> three</RouterLink></li>
-                <li><RouterLink to="/CompositionAPI/lession4"> Four </RouterLink></li>
-                <li><RouterLink to="/CompositionAPI/lession5"> Five </RouterLink></li>
-                <li><RouterLink to="/CompositionAPI/lession6"> six</RouterLink></li>
-                <li><RouterLink to="/CompositionAPI/lession7"> seven</RouterLink></li>
-                <li><RouterLink to="/CompositionAPI/lession8"> eight</RouterLink></li>
-                <li><RouterLink to="/CompositionAPI/lession9"> Nine </RouterLink></li>
-                <li><RouterLink to="/CompositionAPI/lession10"> ten</RouterLink></li>
-                <li><RouterLink to="/CompositionAPI/lession11"> Eleven</RouterLink></li>
-                <li><RouterLink to="/CompositionAPI/lession12"> Twelve</RouterLink></li>
-                <li><RouterLink to="/CompositionAPI/lession13"> Thirtee</RouterLink></li>
-                <li><RouterLink to="/CompositionAPI/lession14"> Fourteen</RouterLink></li>
-              </ul>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </header> -->
     <NavigationView />
     </div>
 </template>
@@ -62,7 +34,7 @@ body {
 }
 
 #app {
-  background: #fff;
+  background: green;
   padding: 2rem;
   margin: 1rem;
   border-radius: 3px;
--- Vue-School-Tutorial/src/views/HomeView.vue
+++
@@ -1,7 +0,0 @@
-<script setup></script>
-
-<template>
-  <main>
-    <h1>Wecome To Vue School Tutorial</h1>
-  </main>
-</template>
